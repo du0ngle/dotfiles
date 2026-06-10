bf3a9aa0660c32c1099b17051bdd8710bc319c47
du0ngle <lqduong81196@gmail.com>
2026-06-10 21:15:08 +0200

    OSC clipb

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -21,6 +21,18 @@ vim.opt.updatetime = 50
 
 vim.opt.clipboard = "unnamedplus"
 
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 vim.g.netrw_bufsettings = 'noma nomod nu nowrap ro nobl'
 
 vim.opt.cursorline = true
--- .config/nvim/lua/lsp/clangd.lua
+++ .config/nvim/lua/lsp/clangd.lua
@@ -1,10 +1,12 @@
 return function()
+  local found = vim.fs.find({ ".clangd", ".git", "compile_commands.json" }, { upward = true })[1]
+  local root = found and vim.fs.dirname(found) or vim.loop.cwd()
+
   vim.lsp.start({
     name = "clangd",
-    cmd = { "clangd" },
-    root_dir = vim.fs.dirname(
-      vim.fs.find({ "compile_commands.json", ".git" }, { upward = true })[1]
-    ),
+    cmd = { "clangd", "--background-index" },
+    root_dir = root,
     filetypes = { "c", "cpp" },
+    single_file_support = true,
   })
 end
